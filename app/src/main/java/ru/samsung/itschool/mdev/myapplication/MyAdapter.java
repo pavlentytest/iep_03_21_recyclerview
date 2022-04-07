2b3cf5eb9defc3505f5d8cce14d6bec0b8d0434c
Pavel <impetus@yandex.ru>
2022-04-07 11:51:12 +0300

    step2

--- app/src/main/java/ru/samsung/itschool/mdev/myapplication/MyAdapter.java
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MyAdapter.java
@@ -1,5 +1,6 @@
 package ru.samsung.itschool.mdev.myapplication;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,22 +9,36 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewRow> {
+
+    private ArrayList<MainActivity.Block> arrayList;
+
+    public MyAdapter(ArrayList<MainActivity.Block> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    // lombok
 
     @NonNull
     @Override
     public ViewRow onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_row,parent,false);
+        return new ViewRow(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewRow holder, int position) {
-
+        holder.textView.setText(arrayList.get(position).getName());
+        Picasso.get().load(arrayList.get(position).getUrl()).into(holder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrayList.size();
     }
 
     class ViewRow extends RecyclerView.ViewHolder {
